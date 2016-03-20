3eab061660f797cab75218b0f01de4c36b5bd388
Richard Udovich <Richard.Udovich@aussiefarmers.com.au>
2016-03-20 23:04:35 +1100

    break it!

--- tests/checkDivBg.js
+++ tests/checkDivBg.js
@@ -10,6 +10,6 @@ describe('getDivBg2', function() {
     var d = document.querySelector('.box');
 
     it('Should be teal2', function() {
-        expect(d.style.backgroundColor).toBe('tealxxx);
+        expect(d.style.backgroundColor).toBe('tealxxx');
     });
 });
